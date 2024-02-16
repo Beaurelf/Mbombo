face617898bbac96d25fa0df176d190edd9c3e2d
Beaurel Fohom <beaurelfohom79@gmail.com>
2024-02-15 21:48:21 -0500

    fix warning

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter,Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter,Routes, Route } from 'react-router-dom';
 import './App.css';
 import './components/Header'
 import Home from './pages/Home';
